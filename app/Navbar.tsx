d06da67e7c8eb175651ed5492ac06b9a198c48af
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-10-24 22:49:44 +0530

    Add: Navbar Created

---
+++ app/Navbar.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { Container, Flex } from "@radix-ui/themes";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
+const Navbar = () => {
+  return (
+    <nav className="border-b mb-5 px-5 py-3">
+      <Container>
+        <Flex justify={"between"}>
+          <Flex align={"center"} gap="3">
+            <Link href={"/"}>
+              <AiFillBug />
+            </Link>
+            <NavLinks />
+          </Flex>
+        </Flex>
+      </Container>
+    </nav>
+  );
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues/list" },
+  ];
+
+  return (
+    <ul className="flex space-x-6 ml-2">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classnames({
+              "!text-zinc-900 font-semibold": link.href === currentPath,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Navbar;
